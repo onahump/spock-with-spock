1993d358915937617f1e78fad50a5a19f2787818
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-05-22 15:02:52 -0500

    Using the operation class

--- src/test/groovy/MedianaTestSpec.groovy
+++ src/test/groovy/MedianaTestSpec.groovy
@@ -1,39 +1,38 @@
 import spock.lang.Specification
+import Operations
 
 class MedianaTestSpec extends Specification{
-	
+
 	def "Getting median from a odd list"() {
-		
+    def median = null
 		given:
-		def list = [6,3,5]
-		def clone_list , median , middle_list = null
+		def list = [3,5,6]
+		Operations operations = new Operations()
 
-		when: 
-		clone_list = list.collect().sort()
-		middle_list = list.size().intdiv(2)
+		when:
 
-		median = list.size()%2 ? clone_list[middle_list] : (clone_list[middle_list - 1] + clone_list[middle_list])/ 2		
+		median = operations.getting_median(list)
 
-		then: 
+		then:
 		assert median == 5
 	}
 
 	def "Getting median from a even list"() {
-		
+
 		given:
 		def list = [6,3,5,5,7,8]
 		def clone_list , median , middle_list = null
 
-		when: 
+		when:
 		clone_list = list.collect().sort()
 		middle_list = list.size().intdiv(2)
 
-		median = list.size()%2 ? clone_list[middle_list] : (clone_list[middle_list - 1] + clone_list[middle_list])/ 2		
+		median = list.size()%2 ? clone_list[middle_list] : (clone_list[middle_list - 1] + clone_list[middle_list])/ 2
 
-		then: 
+		then:
 		assert median == 5.5
 	}
 
 }
 
- 
+
