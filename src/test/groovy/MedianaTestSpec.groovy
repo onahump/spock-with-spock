40160c305db8b80665b6475ca90d64a02712085c
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-05-22 11:31:45 -0500

    Obteniendo la mediana de una lista impar

--- src/test/groovy/MedianaTestSpec.groovy
+++ src/test/groovy/MedianaTestSpec.groovy
@@ -2,16 +2,20 @@ import spock.lang.Specification
 
 class MedianaTestSpec extends Specification{
 	
-	def "getting median from a number list"() {
+	def "Getting median from a odd list"() {
 		
 		given:
 		def list = [6,3,5]
+		def clone_list , median , middle_list = null
 
 		when: 
-		list = list.sort()
-		println list 
+		clone_list = list.collect().sort()
+		middle_list = list.size().intdiv(2)
+
+		median = list.size()%2 ? clone_list[middle_list] : (clone_list[middle_list - 1] + clone_list[middle_list])/ 2		
+
 		then: 
-		assert list == [3,5,6]
+		assert median == 5
 	}
 
 }
