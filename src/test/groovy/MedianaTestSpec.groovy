86e98b052f2441632f135f2f79f4617c7dbea0ff
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-05-22 11:32:12 -0500

    Obteniendo la mediana de una lista par

--- src/test/groovy/MedianaTestSpec.groovy
+++ src/test/groovy/MedianaTestSpec.groovy
@@ -18,6 +18,22 @@ class MedianaTestSpec extends Specification{
 		assert median == 5
 	}
 
+	def "Getting median from a even list"() {
+		
+		given:
+		def list = [6,3,5,5,7,8]
+		def clone_list , median , middle_list = null
+
+		when: 
+		clone_list = list.collect().sort()
+		middle_list = list.size().intdiv(2)
+
+		median = list.size()%2 ? clone_list[middle_list] : (clone_list[middle_list - 1] + clone_list[middle_list])/ 2		
+
+		then: 
+		assert median == 5.5
+	}
+
 }
 
  
